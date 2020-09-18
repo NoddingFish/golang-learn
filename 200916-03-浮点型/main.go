bcab82846fbaf2d24a2bbd957c94ffc4ba421f4b
打盹的鱼 <nlxuran@163.com>
2020-09-18 18:16:41 +0800

    float

--- 200916-03-浮点型/main.go
+++ 200916-03-浮点型/main.go
@@ -16,5 +16,23 @@ func main() {
 	fmt.Println("price=", price)
 	fmt.Println("price2=", price2)
 
+	//Golang 的浮点型默认是 float64
+	var price3 = 1.2
+
+	fmt.Printf("Golang 的浮点型默认：%T \n", price3)
+
+
+	num1 := 1.1
+	num2 := .221 // .221 => 0.221
+
+	fmt.Println("num1=", num1, "num2=", num2)
+
+	num3 := 2.21454e3 // 2.21 * 10的3次方
+	num4 := 2.21454E3 // 2.21 * 10的3次方
+	num5 := 2.21454E-1 // 2.21 * 10的3次方
+
+	fmt.Println("num3=", num3)
+	fmt.Println("num4=", num4)
+	fmt.Println("num5=", num5)
 
 }

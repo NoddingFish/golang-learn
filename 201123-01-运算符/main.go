447f9393aa3d71491bd799a946511e46a69a14e5
打盹的鱼 <nlxuran@163.com>
2020-11-23 17:54:46 +0800

    运算符

---
+++ 201123-01-运算符/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt" //如果我们没有使用一个包，但是不想去掉，前面加 _
+)
+
+func main() {
+	//TODO 算术运算符
+	var num int
+	var num2 int = 64
+	var num3 int = 31
+	num = num2 - num3
+
+	fmt.Println("结果：", num)
+
+	//TODO 赋值预算福
+
+	//TODO 比较运算符
+
+	//TODO 逻辑运算符
+
+	//TODO 位运算符
+
+	//TODO 其他运算符
+}

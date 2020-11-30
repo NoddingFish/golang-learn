421725769f9c76809a5f554bee932e1456decc47
徐然 <nlxuran@163.com>
2020-12-01 00:37:21 +0800

    多重循环

--- 201129-09-程序流程控制-循环控制/main.go
+++ 201129-09-程序流程控制-循环控制/main.go
@@ -24,4 +24,42 @@ func main() {
 			break
 		}
 	}
+
+	//使用细节
+	//TODO 遍历字符串-方法一
+	//一个汉字在 utf-8 编码中占3个字节，解决：将 str 转成 []rune 切片
+	var str1 string = "hello背景"
+	str11 := []rune(str1) // 解决方法
+	for i := 0; i < len(str11); i++ {
+		fmt.Printf("%c \n", str11[i])
+	}
+
+	//TODO 遍历字符串-方法二 for-range
+	//默认安装字符的方式遍历，有中文也是可以的
+	var str2 string = "world你好中国"
+	for index, val := range str2 {
+		fmt.Printf("index=%d val=%c \n", index, val)
+	}
+
+	//练习一
+	var count int = 0
+	var sum int = 0
+	for i := 1; i <= 100; i++ {
+		if i % 9 == 0  {
+			count++
+			sum += i
+		}
+	}
+	fmt.Printf("count=%d sum=%d \n", count, sum)
+
+	//练习二
+	var num1 int
+	fmt.Println("请输入一个数：")
+	fmt.Scanln(&num1)
+	for i := 0; i <= num1; i++ {
+		fmt.Printf("%d + %d = %d \n", i, num1 - i, num1)
+	}
+
+	//golang 中没有 while 和 do ... while
+	
 }
---
+++ 201130-01-程序流程控制-多重循环控制/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt" //如果我们没有使用一个包，但是不想去掉，前面加 _
+)
+
+func main() {
+	//多重循环控制 - 一般使用两层
+	//练习一
+	// allSum := 0.0
+	// for i := 0; i < 3; i++ {
+	// 	sum := 0.0
+	// 	for j := 0; j < 5; j++ {
+	// 		var score float64
+	// 		fmt.Printf("请输入第%d个学生的成绩：", j+1)
+	// 		fmt.Scanln(&score)
+	// 		sum += score
+	// 	}
+	// 	fmt.Printf("班级 %d 的平均分是 %v \n", i, sum / 5)
+	// 	allSum += sum
+	// }
+	// fmt.Printf("所有班级的总平均分是%v \n", allSum / 15)
+
+	//练习二
+	//打印金字塔
+	var jinzita1 int
+	fmt.Printf("输入金字塔（一）的层级：")
+	fmt.Scanln(&jinzita1)
+	for i := 1; i <= jinzita1; i++ {
+		for j := 0; j < i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+
+	//练习三
+	//打印金字塔
+	var jinzita2 int
+	fmt.Printf("输入金字塔（二）的层级：")
+	fmt.Scanln(&jinzita2)
+	for i := 1; i <= jinzita2; i++ {
+		for j := 0; j < jinzita2 - i; j++ {
+			fmt.Print(" ")
+		}
+		for j := 0; j < 2 * i -1; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+
+	
+	//练习四
+	//打印空心金字塔
+	var jinzita3 int
+	fmt.Printf("输入金字塔（三）的层级：")
+	fmt.Scanln(&jinzita3)
+	for i := 1; i <= jinzita3; i++ {
+		for j := 0; j < jinzita3 - i; j++ {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= 2 * i - 1; j++ {
+			if j == 1 || j == 2 * i - 1 || i == jinzita3 {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}

6c7f34743187b9aee4101124b8ae33bf9605c9b4
打盹的鱼 <nlxuran@163.com>
2020-12-29 19:18:21 +0800

    文件copy

--- 14-文件操作/201228-01-文件操作/7-拷贝文件/main.go
+++ 14-文件操作/201228-01-文件操作/7-拷贝文件/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
-	_ "fmt"
-	_ "io/ioutil"
+	"fmt"
+	"os"
+	"io"
+	"bufio"
 )
 
-// func CopyFile(dstFile string, srcFile string) (written int64, err error) {
+func CopyFile(dstFileName string, srcFileName string) (written int64, err error) {
+	srcFile, err := os.Open(srcFileName)
+	if err != nil {
+		fmt.Println("open file err=", err)
+	}
+	reader := bufio.NewReader(srcFile)
 	
-// }
+	defer srcFile.Close()
+
+	// dstFileName 可能不存在
+	dstFile, err := os.OpenFile(dstFileName, os.O_WRONLY | os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("open file err=", err)
+	}
+
+	writer := bufio.NewWriter(dstFile)
+
+	defer dstFile.Close()
+
+	return io.Copy(writer, reader)
+}
 
 func main()  {
-	
+	srcFileName := "../file/avatar.jpg"
+	dstFileName := "../file/avatar2.jpg"
+	_, err := CopyFile(dstFileName, srcFileName)
+	if err == nil {
+		fmt.Println("复制完成...")
+	} else {
+		fmt.Println("复制错误：", err)
+	}
 }
